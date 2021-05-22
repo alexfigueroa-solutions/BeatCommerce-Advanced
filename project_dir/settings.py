0a837bd31e8065be833014fccccab3e8e89b3a09
Alejandro Figueroa <alexfigueroa.cybr@gmail.com>
2021-05-22 14:56:14 -0700

    Added wepack functionality to Django project

--- project_dir/settings.py
+++ project_dir/settings.py
@@ -74,9 +74,21 @@ WSGI_APPLICATION = 'project_dir.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+
+        'NAME': "postgres",
+
+        'USER': 'postgres',
+
+        'PASSWORD': 'techMa$ter2500',
+
+        'HOST': 'localhost',
+
+        'PORT': '5432',
+
     }
 }
 
